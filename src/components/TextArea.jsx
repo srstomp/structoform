e738c12c65a716a9de22c4ffc70064df37f01334
Steve Stomp <steve@codeandcoding.com>
2019-10-09 16:23:47 +0200

    feat: Added textarea

---
+++ src/components/TextArea.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import { id } from '../constants/helper';
+
+const TextArea = ({label, name, value = '', direction, type, placeholder, onChange, errorMessage, showError = false,
+                       className = ""}) =>
+    <div className={`form-item form-item${direction} ${className}`}>
+        {label !== '' ? <label className={`form-item__label`} htmlFor={id}>{label}</label> : null}
+        <div className="form-item__inner">
+            <textarea className={`form-item__textarea ${showError ? 'error' : ''}`} placeholder={placeholder}
+                      onChange={onChange} name={name} htmlFor={id}>
+                { value }
+            </textarea>
+            <span className={`form-item__error-label ${showError ? '' : 'hide'}`}>{errorMessage}</span>
+        </div>
+    </div>
+
+export default TextArea
+
+TextArea.direction = {
+    row: '--row',
+    column: '--column',
+}
+
+TextArea.propTypes = {
+    label: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    direction: PropTypes.oneOf(Object.values(TextField.direction)),
+    type: PropTypes.string.isRequired,
+    placeholder: PropTypes.string,
+    onChange: PropTypes.func,
+    errorMessage: PropTypes.string,
+    showError: PropTypes.bool
+}
