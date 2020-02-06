efe14dcdc7c7b339c98c15d8957daa938b753180
Robin de Bondt <robo@tryfirst.nl>
2020-02-06 15:18:38 +0100

    feat: Added jsonConfig prop to Form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,9 +4,19 @@ import PropTypes from 'prop-types'
 import { TextField, SelectField, DateField, Checkbox, TextArea, RadioButtonGroup, useForm } from "../index"
 import { direction } from '../constants/helper'
 
-const Form = ({ className = '', layout, layoutDirection, initValues = [], submitButton, onSubmit }) => {
+const Form = ({ jsonConfig = '{}', className = '', layout, layoutDirection, initValues = [], submitButton, onSubmit }) => {
 
-    const validationRules = {...layout}
+    const parsedConfig = jsonConfig ? JSON.parse(jsonConfig) : {}
+
+    const config = {
+        className,
+        layout,
+        layoutDirection,
+        initValues,
+        ...parsedConfig,
+    }
+
+    const validationRules = {...config.layout}
 
     Object.keys(validationRules).map((item, i) =>
         validationRules[item] = {type: validationRules[item].type, rules: validationRules[item].validators}
@@ -14,12 +24,12 @@ const Form = ({ className = '', layout, layoutDirection, initValues = [], submit
 
     const { values, errors, handleSubmit, handleChange } = useForm(() => submit(), validationRules)
 
-    const dir = layoutDirection === 'row' ? direction.row : direction.column
+    const dir = config.layoutDirection === 'row' ? direction.row : direction.column
 
     const getValue = (key) => {
         // If no inputes are given, then return default valutes or empty string
         if (!values[key]) {
-            return initValues[key] || ''
+            return config.initValues[key] || ''
         }
         return values[key]
     }
@@ -64,10 +74,10 @@ const Form = ({ className = '', layout, layoutDirection, initValues = [], submit
     }
 
     return (
-        <form className={`form ${className}`} onSubmit={handleSubmit} noValidate >
+        <form className={`form ${config.className}`} onSubmit={handleSubmit} noValidate >
             {
-                Object.keys(layout).map((key, i) => {
-                    return getItem(key, layout[key], i)
+                Object.keys(config.layout).map((key, i) => {
+                    return getItem(key, config.layout[key], i)
                 })
             }
             { submitButton }
@@ -78,8 +88,14 @@ const Form = ({ className = '', layout, layoutDirection, initValues = [], submit
 export default Form
 
 Form.propTypes = {
+    jsonConfig: PropTypes.string,
     className: PropTypes.string,
-    layout: PropTypes.object.isRequired,
+    layout: PropTypes.object,
     layoutDirection: PropTypes.string,
     initValues: PropTypes.array,
+    submitButton: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ]),
+    onSubmit: PropTypes.func
 }
