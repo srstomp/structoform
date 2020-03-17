cb367f8808c140c746579f08ee263958056afb26
Robin de Bondt <robo@tryfirst.nl>
2020-03-17 11:06:40 +0100

    Revert "fix: Invalid prop inputMode on input component in TextField"
    This reverts commit 9f671a4fef052e69314a4274f6c583a0c0ed3c3f.

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -14,6 +14,19 @@ const TextField = ({label, name, placeholder, value, direction, type, errorMessa
 
     const handleChange = e => setCurrentValue(e.target.value)
 
+    const inputMode = () => {
+        switch (type) {
+            case 'email':
+                return 'email'
+            case 'phone':
+                return 'tel'
+            case 'number':
+                return 'numeric'
+            default:
+                return 'latin'
+        }
+    }
+
     return isVisible && (
         <FormItem label={label} id={id} direction={direction}>
             <input className={`form-item__input ${showError ? 'error' : ''}`}
@@ -22,7 +35,7 @@ const TextField = ({label, name, placeholder, value, direction, type, errorMessa
                    onChange={handleChange}
                    name={name} htmlFor={id}
                    value={currentValue}
-            />
+                   inputMode={inputMode}/>
             <span className={`error-label ${showError ? '' : 'hide'}`}>{errorMessage}</span>
         </FormItem>
     )
