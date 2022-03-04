43b31f27ad6bee87a560bd08aaef769b4fd4a7cf
Steve Stomp <stomp@im3d.nl>
2022-03-04 15:17:27 +0100

    refactor: Changed props of PasswordField

--- src/components/PasswordField.jsx
+++ src/components/PasswordField.jsx
@@ -1,57 +1,61 @@
-import React, {useState, useRef} from 'react';
-import PropTypes from "prop-types";
-import { id } from '../constants/helper';
-import FormItem from "./FormItem";
+import React, { useState, useRef, useEffect } from 'react'
+import PropTypes from 'prop-types'
 
-const PasswordField = ({label, name, value, direction, type, placeholder, onChange, errorMessage, showError, toggleLabelShow, toggleLabelHide}) => {
+const PasswordField = ({id, name, placeholder, value, type, showError, onChange, toggleLabelShow, toggleLabelHide}) => {
     const [passwordIsShown, setPasswordIsShown] = useState(false)
     const inputEl = useRef(null)
+    const [currentValue, setCurrentValue] = useState(value)
+
+    useEffect(() => {
+        onChange(name, currentValue)
+    }, [currentValue])
+
+    const handleChange = e => {
+        setCurrentValue(e.target.value)
+    }
 
     const togglePassword = () => {
-         inputEl.current.type = inputEl.current.type === 'password' ? 'text' : 'password'
-         setPasswordIsShown(inputEl.current.type === 'text')
-     }
-
-     const passwordToggleButton = () => {
-         return (
-             type === 'password' ?
-                 <button className={`form-item__input-button ${!passwordIsShown ? 'active' : ''}`} onClick={togglePassword}>
-                     {
-                         !passwordIsShown ? toggleLabelShow : toggleLabelHide
-                     }
-                 </button> : null
-         )
-     }
+        inputEl.current.type = inputEl.current.type === 'password' ? 'text' : 'password'
+        setPasswordIsShown(inputEl.current.type === 'text')
+    }
 
     return (
-        <FormItem label={label} direction={direction}>
-            <div className="form-item__inner" style={{display: 'flex', alignItems: 'center'}}>
-                <input className={`form-item__input ${showError ? 'error' : ''}`} placeholder={placeholder}
-                       ref={inputEl} type={type} onChange={onChange} name={name} htmlFor={id} value={value}/>
-                {passwordToggleButton()}
-                <span className={`error-label ${showError ? '' : 'hide'}`}>{errorMessage}</span>
-            </div>
-        </FormItem>
+        <div className="form-item__text-wrapper" style={{display: 'flex', alignItems: 'center'}}>
+            <input
+                className={`form-item__input ${showError ? 'error' : ''}`}
+                placeholder={placeholder}
+                type={type}
+                onChange={handleChange}
+                name={name}
+                htmlFor={id}
+                value={currentValue}
+                ref={inputEl}
+                onBlur={() => setCurrentValue(currentValue)}
+            />
+            {
+                type === 'password' ?
+                    <button className={`form-item__input-button ${!passwordIsShown ? 'active' : ''}`}
+                            onClick={togglePassword}>
+                        {
+                            !passwordIsShown ? toggleLabelShow : toggleLabelHide
+                        }
+                    </button> : null
+            }
+            <span className={`error-label ${showError ? '' : 'hide'}`}>{errorMessage}</span>
+        </div>
     )
 }
 
 export default PasswordField
 
-PasswordField.direction = {
-    row: '--row',
-    column: '--column',
-}
-
 PasswordField.propTypes = {
-    label: PropTypes.string.isRequired,
-    toggleLabelShow: PropTypes.string,
-    toggleLabelHide: PropTypes.string,
+    id: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
-    value: PropTypes.string.isRequired,
-    direction: PropTypes.oneOf(Object.values(PasswordField.direction)),
-    type: PropTypes.string.isRequired,
     placeholder: PropTypes.string,
-    onChange: PropTypes.func,
-    errorMessage: PropTypes.string,
-    showError: PropTypes.bool.isRequired
+    value: PropTypes.string,
+    type: PropTypes.string.isRequired,
+    showError: PropTypes.bool,
+    onChange: PropTypes.func.isRequired,
+    toggleLabelShow: PropTypes.string,
+    toggleLabelHide: PropTypes.string
 }
